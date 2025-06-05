0a72616ab85477ddf45ede221f6bfcc474133df0
Liam Dubay <liam.dubay@gmail.com>
2025-06-05 10:21:44 +0200

    random updates

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -3,7 +3,15 @@ Utility functions and classes for many scripts.
 """
 
 import numpy as np
+import pandas as pd
+from astropy.table import Table
+import astropy.units as u
+import astropy.coordinates as coords
 
+
+# =============================================================================
+# DATA UTILITY FUNCTIONS
+# =============================================================================
 
 def get_bin_centers(bin_edges):
     """
@@ -25,10 +33,48 @@ def get_bin_centers(bin_edges):
     else:
         raise ValueError('The length of bin_edges must be at least 2.')
 
+
+def fits_to_pandas(path, **kwargs):
+    """
+    Import a table in the form of a FITS file and convert it to a pandas
+    DataFrame.
+
+    Parameters
+    ----------
+    path : Path or str
+        Path to fits file
+    Other keyword arguments are passed to astropy.table.Table
+
+    Returns
+    -------
+    df : pandas DataFrame
+    """
+    # Read FITS file into astropy table
+    table = Table.read(path, format='fits', **kwargs)
+    # Filter out multidimensional columns
+    cols = [name for name in table.colnames if len(table[name].shape) <= 1]
+    # Convert byte-strings to ordinary strings and convert to pandas
+    df = decode(table[cols].to_pandas())
+    return df
+
+
+def decode(df):
+    """
+    Decode DataFrame with byte strings into ordinary strings.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+    """
+    str_df = df.select_dtypes([object])
+    str_df = str_df.stack().str.decode('utf-8').unstack()
+    for col in str_df:
+        df[col] = str_df[col]
+    return df
+
 # =============================================================================
 # PLOTTING FUNCTIONS
 # =============================================================================
-
 
 def get_color_list(cmap, bins):
     """
@@ -48,3 +94,50 @@ def get_color_list(cmap, bins):
     rmin, rmax = bins[0], bins[-2]
     colors = cmap([(r-rmin)/(rmax-rmin) for r in bins[:-1]])
     return colors
+
+# =============================================================================
+# SCIENCE FUNCTIONS
+# =============================================================================    
+
+def galactic_to_galactocentric(l, b, distance):
+    r"""
+    Use astropy's SkyCoord to convert Galactic (l, b, distance) coordinates
+    to galactocentric (r, phi, z) coordinates.
+
+    Parameters
+    ----------
+    l : array-like
+        Galactic longitude in degrees
+    b : array-like
+        Galactic latitude in degrees
+    distance : array-like
+        Distance (from Sun) in kpc
+
+    Returns
+    -------
+    galr : numpy array
+        Galactocentric radius in kpc
+    galphi : numpy array
+        Galactocentric phi-coordinates in degrees
+    galz : numpy arraay
+        Galactocentric z-height in kpc
+    """
+    l = np.array(l)
+    b = np.array(b)
+    d = np.array(distance)
+    if l.shape == b.shape == d.shape:
+        if not isinstance(l, u.quantity.Quantity):
+            l *= u.deg
+        if not isinstance(b, u.quantity.Quantity):
+            b *= u.deg
+        if not isinstance(d, u.quantity.Quantity):
+            d *= u.kpc
+        galactic = coords.SkyCoord(l=l, b=b, distance=d, frame=coords.Galactic())
+        galactocentric = galactic.transform_to(frame=coords.Galactocentric())
+        galactocentric.representation_type = 'cylindrical'
+        galr = galactocentric.rho.to(u.kpc).value
+        galphi = galactocentric.phi.to(u.deg).value
+        galz = galactocentric.z.to(u.kpc).value
+        return galr, galphi, galz
+    else:
+        raise ValueError('Arrays must be of same length.')
